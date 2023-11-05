2b295b9ad0956310f7e2604230bed131258b428b
Varyzgin <dima5varyzgin@gmail.com>
2023-11-05 23:15:30 +0300

    hw4 part

--- src/main/java/edu/project2/MazeGenerator.java
+++ src/main/java/edu/project2/MazeGenerator.java
@@ -1,163 +1,76 @@
 package edu.project2;
 
-public class common {
-    data initGeneratorData(int width, int height, cell startPoint){ //used
-        data d;
-        d.width = width;
-        d.height = height;
-        d.startPoint.x = startPoint.x;
-        d.startPoint.y = startPoint.y;
-        d.maze = init(width, height);
-        d.maze = setMode(d.startPoint, d.maze, GENVISITED);
-        d.s = NULL;
-        d.stackSize = 0;
-        d.unvisitedNum = unvisitedCount(width, height, d.maze);
-        d.error = 0;
-        return d;
+import java.util.Arrays;
+import java.util.Random;
+
+public class MazeGenerator {
+    private final int width;
+    private final int height;
+    private final int[][] maze;
+
+    public MazeGenerator(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.maze = new int[height][width];
     }
 
-    data initSeekerData(data d, cell startPoint, cell exitPoint){
-        d.maze = finalize(d.width, d.height, d.maze);
-        d.startPoint = startPoint;
-        d.exitPoint  = exitPoint;
-        d.s = NULL;
-        d.stackSize = 0;
-        d.error = 0;
-        return d;
-    }
-
-    renderData initRenderData(data d, int windowW, int windowH){
-        renderData rd;
-        rd.width = d.width;
-        rd.height = d.height;
-        rd.x = -windowW/2;
-        rd.y = -windowH/2;
-        GLfloat pSizeX = (GLfloat)(windowW) / (rd.width) ;
-        GLfloat pSizeY = (GLfloat)(windowH) / (rd.height);
-        if(pSizeX > pSizeY) rd.pSize = pSizeY;
-        else                rd.pSize = pSizeX;
-
-        int marginX = (windowW - (rd.pSize * rd.width)) / 2;
-        int marginY = (windowH - (rd.pSize * rd.height)) / 2;
-
-        rd.x += marginX;
-        rd.y += marginY;
-
-        vertex*      vArray = malloc(rd.width*rd.height*4*sizeof(vertex));
-        vertexColor* cArray = malloc(rd.width*rd.height*4*sizeof(vertexColor));
-        vertexColor  defaultColor = {0,0,0};
-        unsigned long long int vert = 0, i, j;
-        for(i = 0; i < rd.height; i++){
-            for(j = 0; j < rd.width; j++){
-                cArray[vert] = defaultColor;
-                vArray[vert].x   = rd.x+rd.pSize*j;
-                vArray[vert++].y = rd.y+rd.pSize*i;
-
-                cArray[vert] = defaultColor;
-                vArray[vert].x   = rd.x+rd.pSize*j;
-                vArray[vert++].y = rd.y+rd.pSize+rd.pSize*i;
-
-                cArray[vert] = defaultColor;
-                vArray[vert].x   = rd.x+rd.pSize+rd.pSize*j;
-                vArray[vert++].y = rd.y+rd.pSize+rd.pSize*i;
-
-                cArray[vert] = defaultColor;
-                vArray[vert].x   = rd.x+rd.pSize+rd.pSize*j;
-                vArray[vert++].y = rd.y+rd.pSize*i;
-            }
-        }
-        rd.vertices = vArray;
-        rd.vertices_count = vert;
-        rd.verticesColor = cArray;
-        return rd;
-    }
-
-    cellString getNeighbours(unsigned int width, unsigned int height, mazeMatrix maze, cell c, int distance){ //used
-        unsigned int i;
-        unsigned int x = c.x;
-        unsigned int y = c.y;
-        cell up = {x, y - distance};
-        cell rt = {x + distance, y};
-        cell dw = {x, y + distance};
-        cell lt = {x - distance, y};
-        cell d[4]  = {dw, rt, up, lt};
-        unsigned int size = 0;
-
-        cellString cells;
-        cells.cells = malloc(4 * sizeof(cell));
-
-        for(i = 0; i < 4; i++){ //для каждого направдения d
-            if(d[i].x > 0 && d[i].x < width && d[i].y > 0 && d[i].y < height){ //если не выходит за границ
-                unsigned int mazeCellCurrent = maze[d[i].y][d[i].x];
-                cell     cellCurrent     = d[i];
-                if(mazeCellCurrent != WALL && mazeCellCurrent != GENVISITED && mazeCellCurrent != SEEKED && mazeCellCurrent != WAY){ //и не посещена\является стеной
-                    cells.cells[size] = cellCurrent; //записать в массив;
-                    size++;
-                }
-            }
+    public int[][] generateMaze() {
+        for (int i = 0; i < height; i++) {
+            Arrays.fill(maze[i], 1); // Заполняем все ячейки стенами (1)
         }
 
-        cells.size = size;
-        return cells;
-    }
-
-    mazeMatrix setMode(cell c, mazeMatrix maze, int mode){ //used
-        unsigned int x = c.x;
-        unsigned int y = c.y;
-        maze[y][x] = mode;
+        divide(0, 0, width, height);
         return maze;
     }
 
-    unsigned int randomRange(unsigned int low, unsigned int high){ //used
+    private void divide(int x, int y, int width, int height) {
+        if (width < 2 || height < 2) {
+            return;
+        }
 
-        return rand() % (high - low + 1) + low;
+        int horizontal = randomInt(width - 1);
+        int vertical = randomInt(height - 1);
+
+        for (int i = y; i < y + height; i++) {
+            maze[i][x + horizontal] = 0; // Создаем проход в горизонтальной стене
+        }
+        for (int i = x; i < x + width; i++) {
+            maze[y + vertical][i] = 0; // Создаем проход в вертикальной стене
+        }
+
+        int wx = randomInt(width);
+        int wy = randomInt(height);
+
+        maze[y + wy][x + wx] = 0; // Создаем проход внутри комнаты
+
+        divide(x, y, horizontal + 1, vertical + 1);
+        divide(x + horizontal + 1, y, width - horizontal - 1, vertical + 1);
+        divide(x, y + vertical + 1, horizontal + 1, height - vertical - 1);
+        divide(x + horizontal + 1, y + vertical + 1, width - horizontal - 1, height - vertical - 1);
     }
 
+    private int randomInt(int max) {
+        Random random = new Random();
+        return random.nextInt(max + 1);
+    }
 
-    int save_png_libpng(const char *filename, uint8_t *pixels, int w, int h)
-    {
-        int i;
-        png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-        if (!png)
-            return 0;
-
-        png_infop info = png_create_info_struct(png);
-        if (!info) {
-            png_destroy_write_struct(&png, &info);
-            return 0;
+    public static void printMaze(int[][] maze) {
+        for (int[] row : maze) {
+            for (int cell : row) {
+                if (cell == 0) {
+                    System.out.print(" "); // Путь
+                } else {
+                    System.out.print("#"); // Стена
+                }
+            }
+            System.out.println();
         }
+    }
 
-        FILE *fp = fopen(filename, "wb");
-        if (!fp) {
-            png_destroy_write_struct(&png, &info);
-            return 0;
-        }
-
-        png_init_io(png, fp);
-        png_set_compression_level(png, 4);
-        png_set_IHDR(png, info, w, h, 8 /* depth */, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE,
-            PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-        png_colorp palette = (png_colorp)png_malloc(png, PNG_MAX_PALETTE_LENGTH * sizeof(png_color));
-        if (!palette) {
-            fclose(fp);
-            png_destroy_write_struct(&png, &info);
-            return 0;
-        }
-        png_set_PLTE(png, info, palette, PNG_MAX_PALETTE_LENGTH);
-        png_write_info(png, info);
-        png_set_packing(png);
-
-        png_bytepp rows = (png_bytepp)png_malloc(png, h * sizeof(png_bytep));
-        for (i = 0; i < h; ++i)
-            rows[i] = (png_bytep)(pixels + (h - i) * w * 4);
-
-        png_write_image(png, rows);
-        png_write_end(png, info);
-        png_free(png, palette);
-        png_destroy_write_struct(&png, &info);
-
-        fclose(fp);
-        free(rows);
-        return 1;
+    public static void main(String[] args) {
+        // I need a week more to complete this project
+        MazeGenerator generator = new MazeGenerator(20, 10);
+        int[][] maze = generator.generateMaze();
+        printMaze(maze);
     }
 }
